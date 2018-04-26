7847d951d788d1abfb2601d572da9d70c18f17bb
vikr01 <vik120@icloud.com>
2018-04-26 14:14:45 -0700

    removing sqlite3 db

--- DropboxPrototype/settings.py
+++ DropboxPrototype/settings.py
@@ -76,10 +76,10 @@ WSGI_APPLICATION = 'DropboxPrototype.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 # Password validation
