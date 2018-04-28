da637be4e24394f72d895df314c9f1bd3bcf89c0
surajsetty97 <surajsetty97@gmail.com>
2018-04-27 19:27:53 -0700

    Added database support

--- DropboxPrototype/urls.py
+++ DropboxPrototype/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('databasetest/', views.createTextBox),
+    path('databasetest/databaseresult/', views.generateResult),
     path('', include('accounts.urls'))
 ]
+
