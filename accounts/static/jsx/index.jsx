0e32fcaa9db0ce8ef077e76a05e1ad91032df155
vikr01 <vikr01@users.noreply.github.com>
2018-05-03 19:58:32 -0700

    code cleanup

--- accounts/static/jsx/index.jsx
+++ accounts/static/jsx/index.jsx
@@ -41,20 +41,7 @@ export default class Place extends React.Component {
 		super(props);
 
 		this.boxes = new Array(size);
-		// this.allboxes = new Array(size*(size+1));
-		
-		// for(let i = 0, k = 0; i < size; i++) {
-		// 	this.boxes[i] = new Array(size);
-		// 	for(let j = 0; j < size; j++, k++) {
-		// 		let key = i.toString()+j.toString();
-		// 		this.allboxes[k] = this.boxes[i][j] = getDiv(key);
-		// 	}
-		// 	this.boxes[i][size-1]=<br key={"linebreak"+i} className={"box"}/>;
-		// }
-
-
 		this.selectedColor="green";
-
 		this.state = {}
 	}
 
@@ -78,30 +65,32 @@ export default class Place extends React.Component {
 		this.refs[key].style.backgroundColor=this.selectedColor;
 	}
 
-	// componentWillMount() {
-	// 	firebase.database().ref(TABLE).once('value').then(
-	// 		function(snapshot) {
-	// 			const table = snapshot.val();
-	// 			_.each(table, function(row, i) {
-	// 				_.each(row, function(col, j) {
-	// 					this.b[getKey(i, j)].style.backgroundColor=col;
-	// 				}.bind(this));
-	// 			}.bind(this));
-	// 		}.bind(this)
-	// 	);
-	// }
+	createPanel() {
+		_.range(size).forEach(function(i) {
+			this.boxes[i] = new Array(size+1);
+			_.range(size).forEach(function(j) {
+				let key = this.getKey(i, j);
+				this.boxes[i][j]=this.getDiv(key);
+			}.bind(this));
+			
+			this.boxes[i][size]=<br 
+				key={"linebreak"+i} 
+				className={"box"}
+			/>;
+		}.bind(this));
+	}
 
-	// componentWillUnmount() {}
-
-	// shouldComponentUpdate(nextProps, nextState) {}
-	// componentDidUpdate(prevProps, prevState, snapshot) {}
-
-	// static getDerivedStateFromProps(nextProps, prevState) {}
-
-	// componentDidCatch(error, info) {}
-
-	// getSnapshotBeforeUpdate(prevProps, prevState) {}
-
+	initializeFromDB() {
+		database.ref(TABLE).once('value').then(
+			function(snapshot) {
+				_.each(snapshot.val(), function(row, i) {
+					_.each(row, function(col, j) {
+						this.refs[this.getKey(i, j)].style.backgroundColor=col;
+					}.bind(this));
+				}.bind(this));
+			}.bind(this)
+		);
+	}
 
 	setListeners() {
 		_.range(size).forEach(
@@ -122,39 +111,12 @@ export default class Place extends React.Component {
 
 
 	render() {
-		_.each(_.range(size), function(i) {
-			this.boxes[i] = new Array(size+1);
-			_.each(_.range(size), function(j) {
-				let key = this.getKey(i, j);
-				this.boxes[i][j]=this.getDiv(key);
-			}.bind(this));
-			this.boxes[i][size]=<br 
-				key={"linebreak"+i} 
-				className={"box"}
-			/>;
-		}.bind(this));
-
-		database.ref(TABLE).once('value').then(
-			function(snapshot) {
-				_.each(snapshot.val(), function(row, i) {
-					_.each(row, function(col, j) {
-						this.refs[this.getKey(i, j)].style.backgroundColor=col;
-					}.bind(this));
-				}.bind(this));
-			}.bind(this)
-		);
-
+		this.createPanel();
+		this.initializeFromDB();
 		this.setListeners();
-		
-
 		
 		return (
 			<div className="boxes_container">
-			{/* <div
-			className={"box"}
-			ref={(r)=>this.r = r}
-			onClick={()=>this.r.style.backgroundColor="red"}
-			/> */}
 				{this.boxes}
 			</div>
 		);
@@ -165,10 +127,3 @@ ReactDOM.render(
 		<Place/>,
 		document.getElementById('react')
 );
-
-function render() {
-	ReactDOM.render(
-		<Place/>,
-		document.getElementById('react')
-	);
-}
