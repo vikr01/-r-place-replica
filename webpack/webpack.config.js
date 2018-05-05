f353523437b66372a62328e78018481c34bca073
vikr01 <vikr01@users.noreply.github.com>
2018-05-05 04:21:13 -0700

    webpack stats

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   },
 
   plugins: [
-    new BundleTracker({ filename: './webpack-stats.json' })
+    new BundleTracker({ filename: './webpack/webpack-stats.json' })
   ],
 
   module: {
