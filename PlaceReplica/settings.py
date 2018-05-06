15993eb4537e0c53b42e4ad99832a46933c3efea
vikr01 <vikr01@users.noreply.github.com>
2018-05-06 15:37:33 -0700

    session engine

--- PlaceReplica/settings.py
+++ PlaceReplica/settings.py
@@ -53,6 +53,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
+
 ROOT_URLCONF = 'PlaceReplica.urls'
 
 TEMPLATES = [
