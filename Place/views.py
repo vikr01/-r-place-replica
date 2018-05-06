7988950d8a95714a6ec3479fce669206cae19bd8
vikr01 <vikr01@users.noreply.github.com>
2018-05-06 04:34:03 -0700

    implemented 30 second cooldown

--- Place/views.py
+++ Place/views.py
@@ -4,6 +4,8 @@ from django.http import JsonResponse
 from colour import Color
 import pyrebase
 import random
+import datetime, dateutil.parser
+import json
 
 CONFIG  = {
 	'apiKey': 'AIzaSyA70V9nqwo5U_mVo4mr8YKprrMyeMaTvuw',
@@ -16,9 +18,9 @@ CONFIG  = {
 
 DATABASE_SIZE = 100
 
-FIREBASE = pyrebase.initialize_app(CONFIG)
+FIREBASE = pyrebase.initialize_app(CONFIG).database()
 
-DB = FIREBASE.database()
+ALLOWED_CHANGES_PER_30_SEC = 5
 
 COLORS = {
     'black',
@@ -55,11 +57,11 @@ def initializeDB():
     """
     Run when resetting database values.
     """
-    #clears DB 
-    # DB.child('grid').remove() 
+    #clears FIREBASE 
+    # FIREBASE.child('grid').remove() 
     for row in range(0, DATABASE_SIZE):
     	for col in range(0, DATABASE_SIZE):
-    		DB.child('grid').child(row).child(col).set(random.choice(tuple(COLORS)))
+    		FIREBASE.child('grid').child(row).child(col).set(random.choice(tuple(COLORS)))
     pass
 
 def createBaseInputPage(request):
@@ -68,6 +70,8 @@ def createBaseInputPage(request):
 
 @csrf_exempt
 def home(request):
+    request.session['changed'] = 0
+    request.session['wait_till'] = (datetime.datetime.now() + datetime.timedelta(0, -1)).isoformat()
     return render(request, 'r-place.html')
 
 def checkColor(color):
@@ -91,32 +95,43 @@ def updatePixelColor(request):
             }
     """
 
-    print('REQUEST: ', request)
-    if not request.POST or not request.POST:
-        print()
+    print('REQUEST: ', str(request))
+
+    if request.session['changed'] == 5:
+        if datetime.datetime.now() < dateutil.parser.parse(request.session['waittill']):
+            return JsonResponse({
+                    False,
+                    None
+            })
+        else:
+            request.session['changed'] = 0
 
     try:
         x = int(request.POST.get('x'))
         y = int(request.POST.get('y'))
-    except ValueError as e:
+        color = request.POST.get('color')
+    except (ValueError, KeyError) as e:
         print(str(e))
         return JsonResponse(createResponse(
                 True,
                 'Invalid value selected.'
         ))
 
-    color = request.POST.get('color')
-
     response = None
     if x < DATABASE_SIZE and x >= 0 \
             and y < DATABASE_SIZE and y >= 0 \
                             and checkColor(color):
         
-        DB.child('grid').child(x).child(y).set(color)
+        FIREBASE.child('grid').child(x).child(y).set(color)
+        request.session['changed'] += 1
         response = createResponse(
             False,
-            None
+            {'wait': True} if request.session['changed'] == 5
+             else {}
         )
+
+        if request.session['changed'] == 5:
+            request.session['waittill'] = (datetime.datetime.now() + datetime.timedelta(0, 30)).isoformat()
     else:
         response = createResponse(True, 'Invalid value selected.')
 
